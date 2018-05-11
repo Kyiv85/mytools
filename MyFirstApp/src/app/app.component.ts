a13df0e3d33a332566faf5c876e19e68e6a1e188
Daniel Delgado <ddelgado@epagos.com.ar>
2018-05-11 11:21:45 -0300

    New test angular

--- MyFirstApp/src/app/app.component.ts
+++ MyFirstApp/src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public title : string;
+  public name : string;
+  public hobbies : string[];
+  public sw : boolean;
+
+  constructor(){
+    this.title = "THISISTHETITLE";
+    this.name = "Daniel Eduardo";
+    this.hobbies = [ 'Run','Read','Develop','Internet'];
+    this.sw = true;
+  }
+
+  public showHobbies(){
+    window.setInterval(this.changeBool(), 2000);
+  }
+
+  private changeBool(){
+    if (this.sw){
+      this.sw=false;
+    }
+    else{
+      this.sw=true;
+    }
+    return this.sw;
+  }
+
+  
 }
